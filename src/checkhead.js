1c710baa627a7d51f60841544daee52824819510
jvail <jvail@users.noreply.github.com>
2018-01-24 15:14:54 +0100

    Try not to rely on HEAD requests

--- src/checkhead.js
+++ src/checkhead.js
@@ -1,27 +1,16 @@
-export default (url) => {
-	return new Promise((resolve, reject) => {
-		/* apparently fetch does not expose all response headers */
-		try {
-			let req = new XMLHttpRequest();
-			req.onload = () => {
-				if (req.status === 200) {
-					resolve({
-						status: req.status,
-						ranges: (req.getResponseHeader('Accept-Ranges') && req.getResponseHeader('Accept-Ranges').indexOf('bytes') >= 0),
-						type: req.getResponseHeader('Content-Type'),
-						length: +req.getResponseHeader('Content-Length')
-					});
-				} else {
-					reject(new Error(`Request failed (${req.status})`));
+export default async (url) => {
+	try {
+		let res = await fetch(url, {
+				method: 'GET',
+				headers: {
+					Range: `bytes=0-1`
 				}
-			};
-			req.onerror = () => {
-				reject(new Error(`Request failed (${req.status})`));
-			};
-			req.open('HEAD', url);
-			req.send();
-		} catch (err) {
-			reject(err);
-		}
-	});
+			});
+		return {
+			type: res.headers.get('Content-Type'),
+			length: res.headers.get('Content-Length') || res.headers.get('Content-Range')
+		};
+	} catch (err) {
+		return Promise.reject(err);
+	}
 };
