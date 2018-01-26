3e7853159dc28e59cfca58dbb8271021d67e820d
jvail <jvail@users.noreply.github.com>
2018-01-26 12:08:56 +0100

    HEAD ok

--- dist/fetchling.js
+++ dist/fetchling.js
@@ -221,15 +221,12 @@ const concat = (bufs) => {
 const checkHead = async (url) => {
 	try {
 		let res = await fetch(url, {
-				method: 'GET',
-				headers: {
-					Range: `bytes=0-1`
-				}
+				method: 'HEAD',
+				mode: 'cors'
 			});
 		return {
 			type: res.headers.get('Content-Type'),
-			length: (res.headers.get('Content-Length') ||
-				res.headers.get('Content-Range')) || Infinity
+			length: res.headers.get('Content-Length') || Infinity
 		};
 	} catch (err) {
 		return Promise.reject(err);
--- src/checkhead.js
+++ src/checkhead.js
@@ -1,15 +1,12 @@
 export default async (url) => {
 	try {
 		let res = await fetch(url, {
-				method: 'GET',
-				headers: {
-					Range: `bytes=0-1`
-				}
+				method: 'HEAD',
+				mode: 'cors'
 			});
 		return {
 			type: res.headers.get('Content-Type'),
-			length: (res.headers.get('Content-Length') ||
-				res.headers.get('Content-Range')) || Infinity
+			length: res.headers.get('Content-Length') || Infinity
 		};
 	} catch (err) {
 		return Promise.reject(err);
